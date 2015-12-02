d3d110d9e04f1547bef2f6b187a23dd0617e0450
Hannah <hurfell@students.uni-mainz.de>
2015-12-02 23:18:55 +0100

    Zufallszahlen, init-Skript fertig gemacht Vorbereitungen fürs Sortieren

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // Variables
-var lists = [{name: "urstapel", values: [123,222,333]},{name: "teststapel", values: [311,122,313]}];
+var anzahlStellen = 3;
+var maxNumber = 4;
+var startNumbers = 5;
+//var lists = [{name: "urstapel", values: [123,222,333]},{name: "teststapel", values: [311,122,313]}];
+var lists = [];
 
 // Program Flow
 
 function initSystem(){
     // Reset all boxes
     deleteAllDivs();
-
     // create new array with numbers
-    createDiv("urstapel","launchPad");
-    createDiv("teststapel","launchPad2");
-    // Reset the machine?
-    alert ("Seite generiert mit " +lists.length + " Elementen.");
-
-
+    createStartDiv();
+    createMachineConfig();
+    console.log("Seite generiert mit " + lists.length + " Elementen.");
 }
 
 function allowDrop(ev) {
@@ -27,8 +27,26 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    alert("Maschine startet... ");
+    console.log(ev.target.id);
+    if (ev.target.id == "theMachine") {
+        alert("Maschine startet... ");
+        ev.target.appendChild(document.getElementById(data));
+        // Start sorting
+
+        // remove urstapel from machine when ready
+
+        // display all new "buckets"
+    }
+    else {
+        alert("Mergevorgang gestartet... ");
+        // We have two divs to merge
+        unteresDiv = ev.target.id;
+        oberesDiv = ev.dataTransfer.getData("text");
+        // start merging
+
+    }
+
+
 }
 
 
@@ -43,14 +61,32 @@ function deleteAllDivs()
 
     });
     // Delete list
-    //lists=[];
+    lists = [];
 }
 
 function deleteDiv(toDelete)
 {
     document.getElementById(toDelete).remove();
 }
+function createStartDiv() {
+    var target = document.getElementById("launchPad");
+    var newDiv = document.createElement('div');
+    var divIdName = "urstapel";
+    newDiv.setAttribute('id', divIdName);
+    newDiv.setAttribute('draggable', 'true');
+    newDiv.setAttribute('ondragstart', 'drag(event)');
+    newDiv.setAttribute('ondragover', 'allowDrop(event)');
+    newDiv.setAttribute('ondrop', 'dropCard(event)');
+    var newNumbers = generateNumbers(startNumbers, anzahlStellen, maxNumber);
+    newNumbers.forEach(function (entry) {
+        newDiv.innerHTML += entry + '<br>';
+    });
+    target.appendChild(newDiv);
 
+    // add the new Array to List
+    addToLists(divIdName, newNumbers);
+
+}
 function createDiv(divId, targetId)
 {
     var target = document.getElementById(targetId);
@@ -60,7 +96,7 @@ function createDiv(divId, targetId)
     newDiv.setAttribute('draggable','true');
     newDiv.setAttribute('ondragstart','drag(event)');
     newDiv.setAttribute('ondragover','allowDrop(event)');
-    newDiv.setAttribute('ondrop','dropCard(event)')
+    newDiv.setAttribute('ondrop', 'dropCard(event)');
     numbers = getNumbers(divIdName);
     numbers.forEach(function(entry){
         newDiv.innerHTML += entry + '<br>';
@@ -70,6 +106,12 @@ function createDiv(divId, targetId)
 
     // add div to list
 
+}
+
+function addToLists(divName, numberArray) {
+    // ToDo: check if divName already is in List
+    lists.push({name: divName, values: numberArray});
+    console.log(lists);
 }
 
 function getNumbers(targetId)
@@ -107,6 +149,42 @@ function merge(unten,oben) {
     var hilf = findElement(lists, 'name',unten.id).values.concat(findElement(lists, 'name',oben).values);
     replaceElement(lists, 'name', unten.id, hilf);
     alert(findElement(lists, 'name',unten.id).values);
+}
+
+function generateNumbers(anzahl, stellen, max) {
+    // return array
+    var zufallsArray = [];
+    var i, j, zahl;
+
+    for (i = 0; i < anzahl; i++) {
+        zahl = "";
+        for (j = 0; j < stellen; j++) {
+            zahl += rand(1, max);
+        }
+        zufallsArray.push(zahl);
+    }
+    console.log(zufallsArray);
+    return zufallsArray
+
+
+}
+
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createMachineConfig() {
+    var target = document.getElementById("machineConfig");
+    var newDiv = document.createElement('input');
+    var divIdName = 'stelle';
+    newDiv.setAttribute('id', divIdName);
+    newDiv.setAttribute('type', 'number');
+    newDiv.setAttribute('name', 'whatNumber');
+    newDiv.setAttribute('min', '1');
+    newDiv.setAttribute('max', "" + anzahlStellen);
+    newDiv.setAttribute('value', "1");
+    target.appendChild(newDiv);
+
 }
 
 
